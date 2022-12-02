48d3f7beb5e85d5dda282f6c869329aaa28becb4
Scott Phillips <polygonguru@gmail.com>
2022-12-02 11:25:31 -0500

    Day 2 works

--- Program.fs
+++ Program.fs
@@ -1,24 +1,7 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
+﻿module aoc_2022
 
 open System
-open System.IO
-
-printfn "Advent of Code"
-
-let readLines filePath = File.ReadLines(filePath);
-let readFile(day: int, demo: bool): seq<string> = 
-    let demoSuffix = if demo then "_demo" else "";
-    readLines(Path.Combine("data",$"day{day}{demoSuffix}.txt"))
-    
-// let chunkSequence(self: seq<string>, split: string): seq<seq<string>> =
-//     let currentSequence = Seq.empty<string>
-//     
-//     for x: string in self do
-//         if x.Equals(split) then
-//             yield currentSequence
-//             currentSequence = []
-//         else
-//             currentSequence = seq { yield! currentSequence; yield x }
+open Utilities
 
 let countCalories(lines: seq<string>): seq<int> =
     let mutable calorieSum = 0
@@ -32,16 +15,60 @@ let countCalories(lines: seq<string>): seq<int> =
         for x in Seq.singleton calorieSum do
             yield x
         }
-    
-let topN(source: seq<_>, N: int): seq<_> =
-    let lowHighSort = Seq.sort source
-    let highLowSort = Seq.rev lowHighSort
-    Seq.take N highLowSort
-    
-let printSequence(source: seq<_>): string =
-    (", ", source) |> String.Join
 
 let day1() =
     let demo = false
-    printfn $"%d{Seq.max(countCalories(readFile(1, demo)))}"
-    printfn $"%d{Seq.sum(topN(countCalories(readFile(1, demo)), 3))}"
+    printfn $"1A: %d{Seq.max(countCalories(readFile(1, demo)))}"
+    printfn $"1B: %d{Seq.sum(topN(countCalories(readFile(1, demo)), 3))}"
+    
+    
+let winMap = Map ["rock", "scissors"; "paper", "rock"; "scissors", "paper";]
+let lossMap = flipMap winMap
+let playMap = Map [ "A", "rock"; "X", "rock"; "B", "paper"; "Y", "paper"; "C", "scissors"; "Z", "scissors"; ]
+let namePlay(x: string): string =
+    playMap[x]
+    
+let playRound(myPlay: string, opponentPlay: string): int =
+    if myPlay.Equals(opponentPlay) then
+        0
+    else if winMap[myPlay].Equals(opponentPlay) then
+        1
+    else
+        -1
+    
+let scorePlay(play: string): int =
+    let scoreMap = Map ["rock", 1; "paper", 2; "scissors", 3]
+    scoreMap[play]
+
+let scoreRound(myPlay: string, opponentPlay: string): int =
+    scorePlay(myPlay) + 3*(1+playRound(myPlay,opponentPlay))
+    
+let playLine(line: string): int =
+    let plays = (line.Split " ") |> Seq.map(namePlay)
+    scoreRound(Seq.last plays, Seq.head plays)
+    
+let strategyPlay(strategy: string, opponentPlay: string) =
+    if strategy.Equals("X") then
+        winMap[opponentPlay]
+    else if strategy.Equals("Y") then
+        opponentPlay
+    else lossMap[opponentPlay]
+    
+let strategyLine(line: string): int =
+    let plays = line.Split " "
+    let opponentPlay = namePlay (Seq.head plays)
+    let myPlay = strategyPlay((Seq.last plays), opponentPlay)
+    scoreRound(myPlay, opponentPlay)
+    
+let playFile(lines: seq<string>, strategy: (string -> int)): seq<int> =
+    lines |> Seq.map(strategy)
+    
+let day2() =
+    let demo = false
+    printfn $"2A: {Seq.sum(playFile(readFile(2, demo), playLine))}"
+    printfn $"2B: {Seq.sum(playFile(readFile(2, demo), strategyLine))}"
+
+
+printfn "AOC 2022"
+day1()
+day2()
---
+++ Utilities.fs
@@ -0,0 +1,22 @@
+﻿module Utilities
+
+open System
+open System.IO
+
+let readLines filePath = File.ReadLines(filePath);
+let readFile(day: int, demo: bool): seq<string> = 
+    let demoSuffix = if demo then "_demo" else "";
+    readLines(Path.Combine("data",$"day{day}{demoSuffix}.txt"))
+    
+let topN(source: seq<_>, N: int): seq<_> =
+    let lowHighSort = Seq.sort source
+    let highLowSort = Seq.rev lowHighSort
+    Seq.take N highLowSort
+    
+let printSequence(source: seq<_>): string =
+    (", ", source  |> Seq.map(string)) |> String.Join
+    
+let flipMap(map: Map<_,_>): Map<_,_> =
+    Map.ofSeq(seq {
+        for key in map.Keys do yield map[key], key
+    })
