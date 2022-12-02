e9ecfc9f769d43410b785354a03cda701d8eaac6
Scott Phillips <polygonguru@gmail.com>
2022-12-02 11:35:03 -0500

    format file

--- Program.fs
+++ Program.fs
@@ -3,72 +3,109 @@
 open System
 open Utilities
 
-let countCalories(lines: seq<string>): seq<int> =
+let countCalories (lines: seq<string>) : seq<int> =
     let mutable calorieSum = 0
+
     seq {
         for line in lines do
             if String.IsNullOrEmpty(line) then
                 yield calorieSum
                 calorieSum <- 0
             else
-                calorieSum <- calorieSum + int(line)
+                calorieSum <- calorieSum + int (line)
+
         for x in Seq.singleton calorieSum do
             yield x
-        }
+    }
 
-let day1() =
+let day1 () =
     let demo = false
-    printfn $"1A: %d{Seq.max(countCalories(readFile(1, demo)))}"
-    printfn $"1B: %d{Seq.sum(topN(countCalories(readFile(1, demo)), 3))}"
-    
-    
-let winMap = Map ["rock", "scissors"; "paper", "rock"; "scissors", "paper";]
+
+    let top3 =
+        function
+        | x -> topN (x, 3)
+
+    printfn $"1A: %d{readFile (1, demo) |> countCalories |> Seq.max}"
+
+    printfn
+        $"1B: %d{readFile (1, demo)
+                 |> countCalories
+                 |> top3
+                 |> Seq.sum}"
+
+let winMap =
+    Map [ "rock", "scissors"
+          "paper", "rock"
+          "scissors", "paper" ]
+
 let lossMap = flipMap winMap
-let playMap = Map [ "A", "rock"; "X", "rock"; "B", "paper"; "Y", "paper"; "C", "scissors"; "Z", "scissors"; ]
-let scoreMap = Map ["rock", 1; "paper", 2; "scissors", 3]
-let namePlay(x: string): string =
-    playMap[x]
-    
-let playRound(myPlay: string, opponentPlay: string): int =
+
+let playMap =
+    Map [ "A", "rock"
+          "X", "rock"
+          "B", "paper"
+          "Y", "paper"
+          "C", "scissors"
+          "Z", "scissors" ]
+
+let scoreMap =
+    Map [ "rock", 1
+          "paper", 2
+          "scissors", 3 ]
+
+let namePlay (x: string) : string = playMap[x]
+
+let playRound (myPlay: string, opponentPlay: string) : int =
     if myPlay.Equals(opponentPlay) then
         0
-    else if winMap[myPlay].Equals(opponentPlay) then
+    else if winMap[ myPlay ].Equals(opponentPlay) then
         1
     else
         -1
-    
-let scorePlay(play: string): int =
-    scoreMap[play]
 
-let scoreRound(myPlay: string, opponentPlay: string): int =
-    scorePlay(myPlay) + 3*(1+playRound(myPlay,opponentPlay))
-    
-let playLine(line: string): int =
-    let plays = (line.Split " ") |> Seq.map(namePlay)
-    scoreRound(Seq.last plays, Seq.head plays)
-    
-let strategyPlay(strategy: string, opponentPlay: string) =
+let scorePlay (play: string) : int = scoreMap[play]
+
+let scoreRound (myPlay: string, opponentPlay: string) : int =
+    scorePlay (myPlay)
+    + 3 * (1 + playRound (myPlay, opponentPlay))
+
+let playLine (line: string) : int =
+    let plays =
+        (line.Split " ") |> Seq.map (namePlay)
+
+    scoreRound (Seq.last plays, Seq.head plays)
+
+let strategyPlay (strategy: string, opponentPlay: string) =
     if strategy.Equals("X") then
         winMap[opponentPlay]
     else if strategy.Equals("Y") then
         opponentPlay
-    else lossMap[opponentPlay]
-    
-let strategyLine(line: string): int =
+    else
+        lossMap[opponentPlay]
+
+let strategyLine (line: string) : int =
     let plays = line.Split " "
     let opponentPlay = namePlay (Seq.head plays)
-    let myPlay = strategyPlay((Seq.last plays), opponentPlay)
-    scoreRound(myPlay, opponentPlay)
-    
-let playFile(lines: seq<string>, strategy: (string -> int)): seq<int> =
-    lines |> Seq.map(strategy)
-    
-let day2() =
+
+    let myPlay =
+        strategyPlay ((Seq.last plays), opponentPlay)
+
+    scoreRound (myPlay, opponentPlay)
+
+let day2 () =
     let demo = false
-    printfn $"2A: {readFile(2, demo) |> Seq.map(playLine) |> Seq.sum}"
-    printfn $"2B: {readFile(2, demo) |> Seq.map(strategyLine) |> Seq.sum}"
+
+    printfn
+        $"2A: {readFile (2, demo)
+               |> Seq.map (playLine)
+               |> Seq.sum}"
+
+    printfn
+        $"2B: {readFile (2, demo)
+               |> Seq.map (strategyLine)
+               |> Seq.sum}"
 
 
 printfn "AOC 2022"
-day1()
-day2()
+day1 ()
+day2 ()
--- Utilities.fs
+++ Utilities.fs
@@ -3,16 +3,23 @@
 open System
 open System.IO
 
-let readLines filePath = File.ReadLines(filePath);
-let readFile(day: int, demo: bool): seq<string> = 
-    let demoSuffix = if demo then "_demo" else "";
-    readLines(Path.Combine("data",$"day{day}{demoSuffix}.txt"))
-    
-let topN(source: seq<_>, N: int): seq<_> =
+let readLines filePath = File.ReadLines(filePath)
+
+let readFile (day: int, demo: bool) : seq<string> =
+    let demoSuffix =
+        if demo then "_demo" else ""
+
+    readLines (Path.Combine("data", $"day{day}{demoSuffix}.txt"))
+
+let topN (source: seq<_>, N: int) : seq<_> =
     Seq.sort source |> Seq.rev |> Seq.take N
-    
-let printSequence(source: seq<_>): string =
-    (", ", source  |> Seq.map(string)) |> String.Join
-    
-let flipMap(map: Map<_,_>): Map<_,_> =
-    seq { for key in map.Keys do yield (map[key], key)} |> Map.ofSeq
+
+let printSequence (source: seq<_>) : string =
+    (", ", source |> Seq.map (string)) |> String.Join
+
+let flipMap (map: Map<_, _>) : Map<_, _> =
+    seq {
+        for key in map.Keys do
+            yield (map[key], key)
+    }
+    |> Map.ofSeq
