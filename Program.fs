2ee7c5ca982dfdc6408e176cf01c41c5a3d2f8f1
Scott Phillips <polygonguru@gmail.com>
2022-12-04 16:43:59 -0500

    day 3 works

--- Program.fs
+++ Program.fs
@@ -20,18 +20,16 @@ let countCalories (lines: seq<string>) : seq<int> =
 
 let day1 () =
     let demo = false
+    let day = 1
+    let lines = readFile (day, demo)
 
     let top3 =
         function
         | x -> topN (x, 3)
 
-    printfn $"1A: %d{readFile (1, demo) |> countCalories |> Seq.max}"
+    printfn $"1A: %d{lines |> countCalories |> Seq.max}"
 
-    printfn
-        $"1B: %d{readFile (1, demo)
-                 |> countCalories
-                 |> top3
-                 |> Seq.sum}"
+    printfn $"1B: %d{lines |> countCalories |> top3 |> Seq.sum}"
 
 let winMap =
     Map [ "rock", "scissors"
@@ -94,18 +92,64 @@ let strategyLine (line: string) : int =
 
 let day2 () =
     let demo = false
+    let day = 2
+    let lines = readFile (day, demo)
+
+    printfn $"2A: {lines |> Seq.map (playLine) |> Seq.sum}"
+
+    printfn $"2B: {lines |> Seq.map (strategyLine) |> Seq.sum}"
+
+
+let splitRucksack (line: string) : string * string =
+    let N = line.Length / 2
+    let a = line.Substring(0, N)
+    let b = line.Substring(N, N)
+    a, b
+
+let uniqueChars (a: string) : Set<char> = Set.ofArray (a.ToCharArray())
+
+let commonCharacter (a: string, b: string) : char =
+    let setA = uniqueChars a
+    let setB = uniqueChars b
+    ((Set.intersect setA setB) |> Set.toList).Head
+
+
+let commonBadge (a: string []) : char =
+    (Seq.ofArray a
+     |> Seq.map (uniqueChars)
+     |> Set.intersectMany
+     |> Set.toList)
+        .Head
+
+
+
+let prioritize (c: char) : int =
+    if Char.IsLower(c) then
+        int c - int 'a' + 1
+    else
+        int c - int 'A' + 27
+
+let day3 () =
+    let demo = false
+    let day = 3
+    let lines = readFile (day, demo)
 
     printfn
-        $"2A: {readFile (2, demo)
-               |> Seq.map (playLine)
+        $"3A: {lines
+               |> Seq.map (splitRucksack)
+               |> Seq.map (commonCharacter)
+               |> Seq.map (prioritize)
                |> Seq.sum}"
 
     printfn
-        $"2B: {readFile (2, demo)
-               |> Seq.map (strategyLine)
+        $"3B: {lines
+               |> Seq.chunkBySize (3)
+               |> Seq.map (commonBadge)
+               |> Seq.map (prioritize)
                |> Seq.sum}"
 
 
 printfn "AOC 2022"
 day1 ()
 day2 ()
+day3 ()
